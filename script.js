a7e20af099796e3557129b70b138617aa1e873c4
Paul <paullechevallier99@gmail.com>
2026-07-21 22:48:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-async function chargerDonnees() {
-    try {
-        const response = await fetch("donnees.json");
-
-        if (!response.ok) {
-            throw new Error("Erreur lors du chargement du JSON");
-        }
-
-        const data = await response.json();
-
-        afficherAteliers(data.ateliers);
-
-    } catch (erreur) {
-        console.error(erreur);
+const ateliers = [
+    {
+        titre: "Cuisine italienne",
+        categorie: "Cuisine",
+        date: "25/07/2026",
+        heure: "14h00",
+        lieu: "Salle A",
+        description: "Apprentissage des pâtes fraîches maison.",
+        places_restantes: 5
+    },
+    {
+        titre: "Initiation au dessin",
+        categorie: "Art",
+        date: "28/07/2026",
+        heure: "10h00",
+        lieu: "Salle B",
+        description: "Découverte des bases du dessin.",
+        places_restantes: 0
+    },
+    {
+        titre: "Yoga débutant",
+        categorie: "Sport",
+        date: "02/08/2026",
+        heure: "18h00",
+        lieu: "Gymnase",
+        description: "Séance accessible aux débutants.",
+        places_restantes: 8
     }
-}
+];
+
 
 function afficherAteliers(ateliers) {
 
@@ -42,9 +56,8 @@ function afficherAteliers(ateliers) {
         `;
 
         container.appendChild(card);
-
     });
-
 }
 
-chargerDonnees();
+
+afficherAteliers(ateliers);
